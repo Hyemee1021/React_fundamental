0ef43ae56fb9c6226bfbb70470931da43cfcc9b6
Hye Mee Park <hyemee1021@gmail.com>
2024-07-15 15:38:08 +1000

    Delete functionaliity and table UI

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import useTodos from "./presenter/useTodos";
-import { Button } from "@mui/material";
 
 const Todo = () => {
   const { list, addList, toggleTodo, delteTodo } = useTodos();
@@ -20,6 +19,12 @@ const Todo = () => {
     setFeedback("Todo added successfully!");
     setTimeout(() => setFeedback(""), 3000);
   };
+
+  const handleDelete = (Id) => {
+    delteTodo(Id);
+    setFeedback("Todo is deleted.");
+    setTimeout(() => setFeedback(""), 3000);
+  };
   return (
     <div className=" m-3 p-4 mx-auto max-w-lg bg-slate-200">
       <div className=" flex items-center justify-center mx-auto gap-4">
@@ -31,7 +36,7 @@ const Todo = () => {
           className="rounded-md w-full p-1"
         />
         <button
-          onclick={handleAddClick}
+          onClick={handleAddClick}
           className="px-3 py-2 rounded-md bg-blue-500 text-white text-sm"
         >
           {" "}
@@ -53,9 +58,44 @@ const Todo = () => {
             {feedback}
           </p>
         )}
-        <ul>
-          {list && list.map((item) => <li key={item.Id}>{item.message}</li>)}
-        </ul>
+
+        <table className="w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50  ">
+            <tr className=" ">
+              <th className=" text-center px-4 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider ">
+                Todo
+              </th>
+              <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Completion
+              </th>
+              <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="bg-white divide-y divide-gray-200">
+            {list &&
+              list.map((item) => (
+                <tr key={item.Id} className=" text-center ">
+                  <td className="  px-4 py-4 text-sm text-gray-900">
+                    {item.message}
+                  </td>
+                  <td className="    px-4 py-4 text-sm text-gray-900">
+                    {item.completed ? "o" : "x"}
+                  </td>
+                  <td className="px-4 py-4 text-sm text-gray-900">
+                    <button
+                      onClick={() => handleDelete(item.Id)}
+                      className="text-red-400 hover:text-red-800"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
